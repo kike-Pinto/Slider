59f45044acfe898ca7126d4eea5bf3c3352263b5
kike-pinto <cripintort@gmail.com>
2023-03-13 17:52:30 -0300

    slider

--- src/exports/images.js
+++ src/exports/images.js
@@ -3,4 +3,4 @@ import image2 from '../assests/2.jpeg'
 import image3 from '../assests/3.jpeg'
 import image4 from '../assests/4.jpeg'
 
-export default [image1, image2, image3, image4, image5]
+export default [image1, image2, image3, image4]
