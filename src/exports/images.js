157317dfc737d30ef237e2fdf4dc4b3020854d5e
kike-pinto <cripintort@gmail.com>
2023-03-13 17:59:22 -0300

    Slider ctm ctm ctm ctm

--- src/exports/images.js
+++ src/exports/images.js
@@ -2,5 +2,6 @@ import image1 from '../assests/1.jpeg'
 import image2 from '../assests/2.jpeg'
 import image3 from '../assests/3.jpeg'
 import image4 from '../assests/4.jpeg'
+import image5 from '../assests/5.jpeg'
 
-export default [image1, image2, image3, image4]
+export default [image1, image2, image3, image4, image5]
